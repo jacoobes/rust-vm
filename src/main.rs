187d2a093319cd83f8d847c81be83e2adad36f22
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2026-07-23 20:29:55 -0500

    music

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,85 @@
 use core::panic;
+use logos::Logos;
 
+#[derive(Logos, Debug, PartialEq)]
+#[logos(skip r"[ \t\n\f]+")] // Ignore this regex pattern between tokens
+#[logos(subpattern decimal = r"[0-9][_0-9]*")]
+enum Token {
+    // Tokens can be literal strings, of any length.
+    #[token("var")]
+    Var,
+    #[token(".")]
+    Period,
+    #[token(":")]
+    Semi,
+    #[token("=")]
+    Equal,
+    #[token("(")]
+    ParenL,
+    #[token(")")]
+    ParenR,
+    #[token("{")]
+    BrackL,
+    #[token("}")]
+    BrackR,
+    #[token("*")]
+    Star,
+    #[token("start")]
+    Start,
 
+    #[token("fn")]
+    Function,
 
-#[derive(Debug)]
-enum Opcode {
-    Halt,
-    Load { dst: usize, val: u32 },
-    Add  { r1: usize, r2: usize, dest: usize},
-    Sub  { r1: usize, r2: usize, dest: usize},
+    #[regex(r"[+-]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?", |lex| lex.slice().parse::<f64>().unwrap())]
+    Double(f64),
+
+    // Or regular expressions.
+    #[regex("[a-zA-Z]+")]
+    Symbol,
 }
-impl From<u32> for Opcode {
-    fn from(instruction: u32) -> Self {
-        let reg: usize = ((instruction  &  0xF0000000) >> 28).try_into().unwrap();
-        let o1: usize  = ((instruction  &  0x0F000000) >> 24).try_into().unwrap();
-        let o2: usize  = ((instruction  &  0x00F00000) >> 20).try_into().unwrap();
-        let o3: usize =  ((instruction  &  0x000F0000) >> 16).try_into().unwrap();
-        let imm = instruction  &  0x0000FFFF;
 
-        match reg {
-            0 => Opcode::Halt,
-            1 => Opcode::Load { dst: o1, val: imm },
-            2 => Opcode::Add  { r1: o1, r2: o2, dest: o3 },
-            3 => Opcode::Sub { r1: o1, r2: o2, dest: o3 },
-            _ => panic!("Unknown instruction")
-        }
-    }
+#[derive(Debug, Clone)]
+enum Expr {
+    Number(f64),
+    Ident(String),
+    Call(String, Vec<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
 }
+ 
+#[derive(Debug, Clone)]
+enum Stmt {
+    VarDecl { name: String, value: Expr },
+    Expr(Expr),
+}
+ 
+#[derive(Debug, Clone)]
+enum Item {
+    VarDecl { name: String, value: Expr },
+    Start(Vec<Stmt>),
+}
+ 
+#[derive(Debug, Clone)]
+struct Program {
+    items: Vec<Item>,
+}
+
+
 
 
 fn main() {
+    let program = "
+        var song = violin2 * sin(0.5)
 
-    let prog: [u32;4] = [ 0x10000064, 0x110000C8, 0x20120000, 0x0];
-    let mut regs: [u32;10] = [0;10];
-
-
-    let mut ip = 0;
-    let mut done = false;
-    while !done {
-        let instruction = Opcode::from(prog[ip]);
-        println!("{:?}", instruction);
-
-        match instruction {
-            Opcode::Halt => { 
-                done = true;
-                ip+=1;
-            },
-            Opcode::Load { dst, val } => {
-                regs[dst]=val;
-                ip+=1;
-            },
-            Opcode::Add { r1, r2, dest } => {
-                let lhs = regs[r1];
-                let rhs = regs[r2];
-                regs[dest]=lhs+rhs;
-                ip+=1;
-            },
-            Opcode::Sub { r1, r2, dest } => { 
-                let lhs = regs[r1];
-                let rhs = regs[r2];
-                regs[dest]=lhs-rhs;
-                ip+=1;
-            }, 
+        start {
+            song()    
         }
 
-        println!("{:?}", regs);
+    ";
+    for result in Token::lexer(program) {
+        match result {
+            Ok(token) => println!("{:#?}", token),
+            Err(e) => panic!("some error occurred: {:?}", e),
+        }
     }
+    
 }
